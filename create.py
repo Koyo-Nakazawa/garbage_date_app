668ff3efb59b4d41a773e96d1dba4432ba56b036
Koyo-Nakazawa <sc.30.32.oj@gmail.com>
2024-08-26 20:25:22 +0900

    Excelデータからデータベース作成完成

--- create.py
+++ create.py
@@ -1,5 +1,7 @@
+import openpyxl.workbook
 from config import Areas, GarbageTyeps, CollectionTypes
 import datetime
+import openpyxl
 
 
 # 地区テーブルのデータ作成
@@ -24,35 +26,43 @@ def create_collection_type(collection_type_id, garbage_type_id, collection_date)
 if __name__ == "__main__":
     # とりあえず一件ずつ作成する処理は完了
     # CSVを読み込んで一括で作成する処理をあとで追加すること
-    areas_data = [
-        ["箱清水一丁目", 1],
-        ["箱清水二丁目", 1],
-        ["高松一丁目", 1],
-        ["高松二丁目", 2],
-        ["高松三丁目", 2],
-        ["上田四丁目", 2],
-        ["高松四丁目", 3],
-        ["上田一丁目", 3],
-        ["上田二丁目", 3]
-        ]
-    garbage_types_data = [
-        "可燃ごみ",
-        "スプレー缶・カセットボンベ",
-        "プラスチックごみ",
-        "不燃ごみ",
-        "びん・カン・ペットボトル",
-        "古紙"
-        ]
 
-    collection_types_data = [
-        [1, 1, datetime.date(2024, 8, 26)],
-        [1, 2, datetime.date(2024, 8, 27)],
-        [1, 3, datetime.date(2024, 8, 27)],
-    
-        ]
-    
+    # Excelからダミーデータを読み込む
+    wb = openpyxl.load_workbook("テーブル定義.xlsx")
 
+    # 地区テーブルの読み込み
+    ws = wb["areas"]
+    cnt = 2
+    while True:
+        area_name = ws.cell(cnt, 2).value
+        collection_type_id = ws.cell(cnt, 3).value
+        if area_name is None:
+            break
+        create_area(area_name, collection_type_id)
+        cnt += 1
 
-    create_area("箱清水", 1)
-    create_garbage_type("可燃ごみ")
-    create_collection_type(1, 1, datetime.date(2024, 8, 26))
+    # ごみ種別テーブルの読み込み
+    ws = wb["garbage_types"]
+    cnt = 2
+    while True:
+        garbage_type_name = ws.cell(cnt, 2).value
+        if garbage_type_name is None:
+            break
+        create_garbage_type(garbage_type_name)
+        cnt += 1
+
+    # 収集種別テーブルの読み込み
+    ws = wb["collection_types"]
+    cnt = 2
+    while True:
+        collection_type_id = ws.cell(cnt, 1).value
+        garbage_type_id = ws.cell(cnt, 2).value
+        collection_date = ws.cell(cnt, 3).value
+        if collection_type_id is None:
+            break
+        create_collection_type(collection_type_id, garbage_type_id, collection_date)
+        cnt += 1
+
+    # create_area("箱清水", 1)
+    # create_garbage_type("可燃ごみ")
+    # create_collection_type(1, 1, datetime.date(2024, 8, 26))
