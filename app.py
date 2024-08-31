d978040a72f512d321e0439eb28ff3464c59f395
Koyo-Nakazawa <sc.30.32.oj@gmail.com>
2024-08-31 21:55:27 +0900

    返信メッセージのテスト

--- app.py
+++ app.py
@@ -23,9 +23,11 @@ from dotenv import load_dotenv
 from read import get_candidate_area
 from reply_text import create_collection_dates_types_reply
 
+
 load_dotenv(override=True)
 
 app = Flask(__name__)
+
 
 line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
@@ -57,27 +59,52 @@ def callback():
     return "OK"
 
 
-# たぶんこの関数をがしがしいじっていく感じだと思われる。
+# テキストメッセージを受け取ったときの処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # ユーザーidをもとにセッションを管理します
     if event.source.user_id not in sessions.keys():
-        sessions[event.source.user_id] = {"flag": False, "area": None}
+        sessions[event.source.user_id] = {"flag": False, "first": True, "area": None}
 
+    # 受け取ったメッセージが「ごみ」のとき
     if event.message.text == "ごみ":
         sessions[event.source.user_id]["flag"] = True
-        candidate_areas = get_candidate_area("箱")
+
+        # 初回であれば、エリア特定のやりとりをする
+        if sessions[event.source.user_id]["first"]:
+            messages = ["町名を入力してください（初回のみ）"]
+
+        # 初回でなければ、収集日の情報を返信する
+        else:
+            messages = create_collection_dates_types_reply(sessions[event.source.user_id]["area"])
+
+        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+    # 受け取ったメッセージが「ごみ」以外のとき
+    # 初回の町名を受け取ったとき
+    elif sessions[event.source.user_id]["first"]:
+        sessions[event.source.user_id]["first"] = False
+        candidate_areas = get_candidate_area(event.message.text)
         items = [
             QuickReplyButton(action=MessageAction(text=f"{area[1]}", label=f"{area[1]}"))
             for area in candidate_areas
         ]
         # クイックリプライオブジェクトを作成
         messages = TextSendMessage(text="地区を選択してください", quick_reply=QuickReply(items=items))
-        line_bot_api.reply_message(event.reply_token, messages=messages)
-    elif sessions[event.source.user_id]["flag"]:
+        # line_bot_api.reply_message(event.reply_token, messages=messages)
+    # 候補地から地区名を受け取ったとき
+    elif sessions[event.source.user_id]["area"] is None:
         sessions[event.source.user_id]["area"] = event.message.text
-        message = create_collection_dates_types_reply(sessions[event.source.user_id]["area"])
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+        messages = [
+            f"あなたの地区を{sessions[event.source.user_id]['area']}に決定しました。\n次回から収集日が出力されます。"
+        ]
+        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+    # 関係ないワードは
+    else:
+        messages = [f"ちょっと何言ってるかわかりません。"]
+        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+    line_bot_api.reply_message(event.reply_token, messages=messages)
 
 
 if __name__ == "__main__":
