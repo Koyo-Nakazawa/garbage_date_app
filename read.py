89b63022bf48f239004b3bc010a4ae0213bdc81f
Koyo-Nakazawa <sc.30.32.oj@gmail.com>
2024-09-06 22:10:34 +0900

    test

--- read.py
+++ read.py
@@ -69,7 +69,7 @@ def output_collection_data(current_area):
         # print(data.collection_date)
         # date = data.collection_date.astimezone(jst)
         date = data.collection_date
-        date = jst.localize(date)
+        date = datetime.datetime(date.year, date.month, date.day, 0, 0, 0)
         # print(date.weekday())
         result.append(
             [
